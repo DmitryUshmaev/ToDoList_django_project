8b87c54e5bd4756aac5bc8e82a38a07d284eb82b
Loncevrot <dmanchester43@gmail.com>
2023-01-23 16:56:30 +0300

    deploy

---
+++ bot/models.py
@@ -0,0 +1,12 @@
+from django.core.validators import MinLengthValidator
+from django.db import models
+
+from core.models import User
+
+
+class TgUser(models.Model):
+    chat_id = models.BigIntegerField(verbose_name='id чата', null=True)
+    user_ud = models.BigIntegerField(verbose_name='id пользователя', null=True)
+    username = models.CharField(max_length=32, validators=[MinLengthValidator(5)])
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.PROTECT, null=True, blank=True)
+    verification_code = models.CharField(max_length=255, null=True, blank=True, verbose_name='Код верификации')
