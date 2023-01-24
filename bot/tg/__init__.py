46a562e4399e43879c1066433fa9ab0e2334e98e
Loncevrot <dmanchester43@gmail.com>
2023-01-24 15:27:42 +0300

    deploy

--- bot/tg/__init__.py
+++ bot/tg/__init__.py
@@ -3,6 +3,6 @@ import os
 from .client import TgClient
 from .dc import get_updates_schema, send_message_schema
 
-tg_client = TgClient(os.environ.get('TG_TOKEN'))
+tg_client = TgClient(os.environ.get('TG_BOT_API_TOKEN'))
 
 __all__ = ("get_updates_schema", "send_message_schema", "tg_client", "TgClient")
