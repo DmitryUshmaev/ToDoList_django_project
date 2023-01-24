00a9431acecb516747080363ffcc9bece7064552
Loncevrot <dmanchester43@gmail.com>
2023-01-24 15:53:42 +0300

    deploy

--- bot/serializers.py
+++ bot/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from rest_framework.exceptions import ValidationError
 from bot.models import TgUser
 from bot.tg import tg_client
 
@@ -14,3 +14,20 @@ class TgUserVerCodSerializer(serializers.ModelSerializer):
         instance = super().update(instance, validated_data)
         tg_client.send_message(chat_id=instance.chat_id, text='Успешно')
         return instance
+
+
+class TgUserSerializer(serializers.ModelSerializer):
+    verification_code = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = TgUser
+        read_only_fields = ('tg_user_id', 'username', 'user_id')
+        fields = ('tg_user_id', 'username', 'verification_code', 'user_id')
+
+    def validate(self, attrs):
+        verification_code = attrs.get('verification_code')
+        tg_user = TgUser.objects.filter(verification_code=verification_code).first()
+        if not tg_user:
+            raise ValidationError({'verification_code': 'field is incorrect'})
+        attrs['tg_user'] = tg_user
+        return attrs
--- bot/views.py
+++ bot/views.py
@@ -2,6 +2,11 @@ from rest_framework import generics
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 
+from rest_framework import permissions
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+
+
 from bot.models import TgUser
 from bot.serializers import TgUserVerCodSerializer
 
@@ -22,3 +27,18 @@ class TgUserUpdate(generics.UpdateAPIView):
     def perform_update(self, serializer):
         serializer.save(user=self.request.user)
 
+class VerificationView(GenericAPIView):
+    model = TgUser
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = TgUserSerializer
+
+    def patch(self, request, *args, **kwargs):
+        s: TgUserSerializer = self.get_serializer(data=request.data)
+        s.is_valid(raise_exception=True)
+
+        tg_user: TgUser = s.validated_data['tg_user']
+        tg_user.user = self.request.user
+        tg_user.save(update_fields=['user'])
+        instance_s: TgUserSerializer = self.get_serializer(tg_user)
+
+        return Response(instance_s.data)
