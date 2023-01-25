55908d544c44a5b5f3c4129423f11d6e9f298df5
Loncevrot <dmanchester43@gmail.com>
2023-01-25 10:13:08 +0300

    deploy

--- bot/models.py
+++ bot/models.py
@@ -10,3 +10,5 @@ class TgUser(models.Model):
     username = models.CharField(max_length=32, validators=[MinLengthValidator(5)], null=True, blank=True, default=None)
     user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.PROTECT, null=True, blank=True)
     verification_code = models.CharField(max_length=255, null=True, blank=True, verbose_name='Код верификации')
+
+##
