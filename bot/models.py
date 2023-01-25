8728f91eecfa55ee21b98781d424bfdc1ec43fc6
Loncevrot <dmanchester43@gmail.com>
2023-01-25 10:07:02 +0300

    deploy

--- bot/models.py
+++ bot/models.py
@@ -6,7 +6,7 @@ from core.models import User
 
 class TgUser(models.Model):
     chat_id = models.BigIntegerField(verbose_name='id чата', null=True)
-    user_ud = models.BigIntegerField(verbose_name='id пользователя', null=True)
-    username = models.CharField(max_length=32, validators=[MinLengthValidator(5)])
+    user_ud = models.BigIntegerField(verbose_name='id пользователя', unique=True, default=None)
+    username = models.CharField(max_length=32, validators=[MinLengthValidator(5)], null=True, blank=True, default=None)
     user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.PROTECT, null=True, blank=True)
     verification_code = models.CharField(max_length=255, null=True, blank=True, verbose_name='Код верификации')
